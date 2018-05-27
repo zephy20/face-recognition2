82c2c6feba15a7e8e1f07553f2ab6868fc92f307
zephy20 <kartikv20@gmail.com>
2018-05-27 16:31:07 +0530

    Add files via upload

---
+++ train.py
@@ -0,0 +1,10 @@
+import cognitive_face as CF
+from global_variables import personGroupId
+
+Key = '0168be88021c4085a479cab9ee593d4f'
+CF.Key.set(Key)
+BASE_URL = 'https://westcentralus.api.cognitive.microsoft.com/face/v1.0/'  # Replace with your regional Base URL
+CF.BaseUrl.set(BASE_URL)
+
+res = CF.person_group.train(personGroupId)
+print res
